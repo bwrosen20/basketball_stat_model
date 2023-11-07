847819be8ad15c0b974c04703eb0ca0afabf6f79
bwrosen20 <bwrosen20@gmail.com>
2023-11-07 18:25:49 -0500

    Loop is ready

--- server/algorithm.py
+++ server/algorithm.py
@@ -22,6 +22,43 @@ with app.app_context():
     '''
 
 
+    #get game data
+
+    schedule_page_url = "https://www.basketball-reference.com/leagues/NBA_2024_games-november.html"
+    schedule_page = requests.get(schedule_page_url, headers = {'User-Agent':"Mozilla/5.0"})
+    schedule = BeautifulSoup(schedule_page.text, 'html.parser')
+
+    monthly_games = schedule.select('tbody')[0].select('tr')
+
+    # current_date = date.today()
+
+    current_date = datetime(2023,11,6)
+
+
+    todays_games = []
+    for game in monthly_games:
+        date = datetime.strptime(game.select('th')[0].text,"%a, %b %d, %Y")
+        if date == current_date:
+            game_data = {}
+            game_data["home"] = game.select('td')[3].text
+            game_data["away"] = game.select('td')[1].text
+            my_time = game.select('td')[0].text
+
+            empty_time = my_time.replace('p','').replace('a','').replace(":","")
+            if my_time[-1]=='p':
+                empty_time = str(int(empty_time)+1200)
+                if int(empty_time) >= 2400:
+                    empty_time = "0"+empty_time[2:4]
+            if int(empty_time)<1000:
+                empty_time = '0'+empty_time
+
+            time = datetime.strptime(empty_time,"%H%M").time()
+            game_data["time"]=time
+
+            todays_games.append(game_data)
+
+    
+
 
     #parse espn injury page
     injury_url = "https://www.espn.com/nba/injuries"
@@ -120,138 +157,152 @@ with app.app_context():
         if odds_dict[item] == {}:
             del(new_odds_dict[item])
 
-    ipdb.set_trace()
-
     #new_odds_dict is a dicitonary of all today's props
 
+    for team_player_name, team_player_odds in new_odds_dict.items():
+
+        player_name = team_player_name
+        #collect my own data about every game they've played in
+        current_player = Player.query.filter(Player.name==player_name).first()
+        games = current_player.games
+
+        last_game = games[-1]
+
+        if last_game.home:
+            player_team = last_game.game.home
+        else:
+            player_team = last_game.game.visitor
 
 
-    #collect data
-    player_home_or_away = True
-    player_name = "Jalen Green"
-    player_team = "Houston Rockets"
-    other_team = "Sacramento Kings"
-    game_time = datetime.time(19,30)
+        game = [game for game in todays_games if game["home"]==player_team or game["away"]==[player_team]][0]
 
-    injury = False
-    opponent_injury = False
-    games_with_injury = []
-    games_with_opp_injury = []
+        if game["home"] == player_team:
+            player_home_or_away = True
+            other_team = game["away"]
+        else:
+            player_home_or_away = False
+            other_team = game["home"]
 
-    #collect my own data about every game they've played in
-    current_player = Player.query.filter(Player.name==player_name).first()
-    games = current_player.games
+        game_time = game["time"]
+        
 
-   
+        injury = False
+        opponent_injury = False
+        games_with_injury = []
+        games_with_opp_injury = []
 
-    #we got the injuries. Time to check the roster
-
-
-
-    #loop of game players if there is an injury and filter down to only games they have not played in. If the list
-    #is not big enough, include recent games but add that players average stats rationed to the rest of the team
-
-    #if the other team has injuries, try to find matchups against them without that player
-    if player_team in injured_list:
-        injury = True
-        games_with_injury = games.copy()
-        #gets all the players in each game
-        for game_players in [game.game.players for game in games]:
-            #this_current_player is the current player's specific PlayerGame
-            this_current_player = [game for game in game_players if game.player.name==current_player.name][0]
-            team_players = [player for player in game_players if player.home==this_current_player.home]
-            team_player_names = [player.player.name for player in team_players]
-            #make a new injured list with only starters
-            
-            for injured_player in (injured_list[player_team]):
-                injured_player_games = Player.query.filter(Player.name==injured_player).first().games[-3:]
-                minutes = mean([game.minutes for game in injured_player_games])
-                if minutes > 2000:
-                    if injured_player in team_player_names and this_current_player in games_with_injury:
-                        games_with_injury.remove(this_current_player)
-
-
-   
-   
-    #latest 5 games at time
-    games_at_time = [game for game in games if game.game.date.time()==game_time][-3:]
-
-
-    #last 5 games
-    latest_games = [game for game in games][-3:]
-    #last 8 home/away games
-    latest_home_or_away_games = [game for game in games if game.home==player_home_or_away][-10:]
+        
 
     
-    #get latest matchups vs team
-    #check recent minutes
-    minutes_list = [game.minutes for game in latest_games]
-    minutes = mean(minutes_list)
-    
-    games_vs_opponent = [game for game in games if (game.game.home==other_team or game.game.visitor==other_team)][-8:]
 
-            
-   
-    
+        #we got the injuries. Time to check the roster
 
-    #check opponent injuries
 
-    if other_team in injured_list:
-        opponent_injury = True
-        games_with_opp_injury = games_vs_opponent.copy()
-        #gets all the players in each game
-        for game_players in [game.game.players for game in games]:
-            #this_current_player is the current player's specific PlayerGame
-            this_current_player = [game for game in game_players if game.player.name==current_player.name][0]
-            opponent_players = [player for player in game_players if player.home!=this_current_player.home]
-            opponent_player_names = [player.player.name for player in opponent_players]
-            #make a new injured list with only starters
-            
-            for injured_player in (injured_list[other_team]):
-                if Player.query.filter(Player.name==injured_player):
-                    injured_player_games = Player.query.filter(Player.name==injured_player).first().games[-5:]
+
+        #loop of game players if there is an injury and filter down to only games they have not played in. If the list
+        #is not big enough, include recent games but add that players average stats rationed to the rest of the team
+
+        #if the other team has injuries, try to find matchups against them without that player
+        if player_team in injured_list:
+            injury = True
+            games_with_injury = games.copy()
+            #gets all the players in each game
+            for game_players in [game.game.players for game in games]:
+                #this_current_player is the current player's specific PlayerGame
+                this_current_player = [game for game in game_players if game.player.name==current_player.name][0]
+                team_players = [player for player in game_players if player.home==this_current_player.home]
+                team_player_names = [player.player.name for player in team_players]
+                #make a new injured list with only starters
+                
+                for injured_player in (injured_list[player_team]):
+                    injured_player_games = Player.query.filter(Player.name==injured_player).first().games[-3:]
                     minutes = mean([game.minutes for game in injured_player_games])
                     if minutes > 2000:
-                        if injured_player in opponent_player_names and this_current_player in games_with_opp_injury:
-                            games_with_opp_injury.remove(this_current_player)
-   
+                        if injured_player in team_player_names and this_current_player in games_with_injury:
+                            games_with_injury.remove(this_current_player)
+
+
+    
+    
+        #latest 5 games at time
+        games_at_time = [game for game in games if game.game.date.time()==game_time][-3:]
+
+
+        #last 5 games
+        latest_games = [game for game in games][-3:]
+        #last 8 home/away games
+        latest_home_or_away_games = [game for game in games if game.home==player_home_or_away][-10:]
+
+        
+        #get latest matchups vs team
+        #check recent minutes
+        minutes_list = [game.minutes for game in latest_games]
+        minutes = mean(minutes_list)
+        
+        games_vs_opponent = [game for game in games if (game.game.home==other_team or game.game.visitor==other_team)][-8:]
+
+                
+    
+        
+
+        #check opponent injuries
+
+        if other_team in injured_list:
+            opponent_injury = True
+            games_with_opp_injury = games_vs_opponent.copy()
+            #gets all the players in each game
+            for game_players in [game.game.players for game in games]:
+                #this_current_player is the current player's specific PlayerGame
+                this_current_player = [game for game in game_players if game.player.name==current_player.name][0]
+                opponent_players = [player for player in game_players if player.home!=this_current_player.home]
+                opponent_player_names = [player.player.name for player in opponent_players]
+                #make a new injured list with only starters
+                
+                for injured_player in (injured_list[other_team]):
+                    if Player.query.filter(Player.name==injured_player):
+                        injured_player_games = Player.query.filter(Player.name==injured_player).first().games[-5:]
+                        minutes = mean([game.minutes for game in injured_player_games])
+                        if minutes > 2000:
+                            if injured_player in opponent_player_names and this_current_player in games_with_opp_injury:
+                                games_with_opp_injury.remove(this_current_player)
+    
 
 
 
-    #make a big array of games
-    latest_home_or_away_games.extend(latest_games)
-    latest_home_or_away_games.extend(games_at_time)
-    latest_home_or_away_games.extend(games_vs_opponent)
-    if injury:
-        latest_home_or_away_games.extend(games_with_injury[-3:])
-    if opponent_injury:
-        latest_home_or_away_games.extend(games_with_opp_injury[-3:])
+        #make a big array of games
+        latest_home_or_away_games.extend(latest_games)
+        latest_home_or_away_games.extend(games_at_time)
+        latest_home_or_away_games.extend(games_vs_opponent)
+        if injury:
+            latest_home_or_away_games.extend(games_with_injury[-3:])
+        if opponent_injury:
+            latest_home_or_away_games.extend(games_with_opp_injury[-3:])
 
-    #find most similar game
-    most_similar = mode(latest_home_or_away_games)
-    assists_similar = most_similar.assists
-    points_similar = most_similar.points
-    trb_similar = most_similar.trb
+        #find most similar game
+        most_similar = mode(latest_home_or_away_games)
+        assists_similar = most_similar.assists
+        points_similar = most_similar.points
+        trb_similar = most_similar.trb
 
 
-    new_game_list = set(latest_home_or_away_games)
+        new_game_list = set(latest_home_or_away_games)
 
-    uniq_game_list = list(new_game_list)
+        uniq_game_list = list(new_game_list)
 
-    uniq_game_list = [game for game in uniq_game_list if (minutes-minutes*.15 <= game.minutes <=minutes+minutes*.9)]
+        uniq_game_list = [game for game in uniq_game_list if (minutes-minutes*.15 <= game.minutes <=minutes+minutes*.9)]
 
-    assists_list = [game.assists for game in uniq_game_list]
-    points_list = [game.points for game in uniq_game_list]
-    trb_list = [game.trb for game in uniq_game_list]
+        assists_list = [game.assists for game in uniq_game_list]
+        points_list = [game.points for game in uniq_game_list]
+        trb_list = [game.trb for game in uniq_game_list]
 
-    assists_factor = mean(assists_list)
-    points_factor = mean(points_list)
-    trb_factor = mean(trb_list)
+        assists_factor = mean(assists_list)
+        points_factor = mean(points_list)
+        trb_factor = mean(trb_list)
 
-    assists_predict = 0.6*assists_factor+0.4*assists_similar
-    points_predict = 0.6*points_factor+0.4*points_similar
-    trb_predict = 0.6*trb_factor+0.4*trb_similar
+        assists_predict = 0.6*assists_factor+0.4*assists_similar
+        points_predict = 0.6*points_factor+0.4*points_similar
+        trb_predict = 0.6*trb_factor+0.4*trb_similar
 
-    ipdb.set_trace()
+        ipdb.set_trace()
 
-    print("hi")
+        print("hi")
